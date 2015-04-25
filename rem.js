2aad204b9f76ce4e90780333e5652f146eeb9b1e
Charlie Cheever <ccheever@gmail.com>
2015-04-24 23:54:34 -0700

    Making rem runnable

---
+++ rem.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+var child_process = require('child_process');
+var path = require('path');
+child_process.execFileSync(path.join(__dirname, './node_modules/.bin/coffee'),
+  [path.join(__dirname, './index.coffee')].concat(process.argv.slice(2)), {
+    stdio: [process.stdin, process.stdout, process.stderr],
+    encoding: 'utf8',
+  });
