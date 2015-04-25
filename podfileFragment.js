b1865f1d14b4fe59bc8a30bb531440e09adee3c8
Charlie Cheever <ccheever@gmail.com>
2015-04-25 01:56:09 -0700

    Moving some stuff around

--- podfileFragment.js
+++ podfileFragment.js
@@ -2,7 +2,7 @@
 var child_process = require('child_process');
 var path = require('path');
 child_process.execFileSync(path.join(__dirname, './node_modules/.bin/coffee'),
-  [path.join(__dirname, './podfile.coffee')].concat(process.argv.slice(2)), {
+  [path.join(__dirname, './src/podfileFragment.coffee')].concat(process.argv.slice(2)), {
     stdio: [process.stdin, process.stdout, process.stderr],
     encoding: 'utf8',
   });
